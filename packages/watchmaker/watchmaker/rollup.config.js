ae3a0f11853a61a7823c206c63c8987339d25f47
Tane Piper <tane-piper@idexx.com>
2021-02-24 21:05:23 +0100

    UMD Terse

--- packages/watchmaker/watchmaker/rollup.config.js
+++ packages/watchmaker/watchmaker/rollup.config.js
@@ -1,18 +1,19 @@
 const { terser } = require('rollup-plugin-terser');
+const iife = require('rollup-plugin-iife');
 
 
 module.exports = (config) => {
 
   return {
     ...config,
-    output: {
-      ...config.output,
-      format: 'cjs',
-      name: 'watchmaker'
-    },
+    // output: {
+    //   ...config.output,
+    //   format: 'cjs',
+    //   name: 'watchmaker'
+    // },
     plugins: [
       ...config.plugins,
-      terser()
+      terser(),
     ]
   };
 };
